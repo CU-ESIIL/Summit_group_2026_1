af1949e26c8e102f6ee1e69dde629690c28b2541
Beatrice Bock <bmb646@nau.edu>
2026-05-14 13:06:30 -0600

    plot

--- code/dummy_plot.R
+++ code/dummy_plot.R
@@ -3,9 +3,10 @@ library(tidyverse)
 
 xs <- seq(0, 10, by = 0.1)
 ys <- seq(20, 0, by = -0.2 )
+ys <- ys + rnorm(length(xs), mean = 0, sd = 1)
 ds <- cbind(xs, ys)
 
-ggplot(ds, aes(x = xs, y = ys)) + geom_line()+ 
+ggplot(ds, aes(x = xs, y = ys)) + geom_smooth()+ 
 geom_point()+
 labs(x = "Knowledge on Energy Usage",
     y = "Energy Usage")+
