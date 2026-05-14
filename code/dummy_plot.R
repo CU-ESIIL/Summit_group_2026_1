d14adab88d79adda52a2bde100ad474bf953a55a
Beatrice Bock <bmb646@nau.edu>
2026-05-14 13:23:35 -0600

    unit3s to plot

--- code/dummy_plot.R
+++ code/dummy_plot.R
@@ -8,8 +8,9 @@ ds <- cbind(xs, ys)
 
 ggplot(ds, aes(x = xs, y = ys)) + geom_smooth()+ 
 geom_point()+
-labs(x = "Knowledge on Energy Usage",
-    y = "Energy Usage")+
+labs(x = "Knowledge on Energy Usage (Unit of Knowledge)",
+    y = "Energy Usage (Unit of Energy Usage)",
+    title = "Team <5'9 Analysis of Energy Knowledge and Energy Usage")+
     theme_bw()
 
-ggsave("energy_use.png", width = 6, height = 4)
+ggsave("docs/assets/figures/energy_use.png", width = 6, height = 4)
