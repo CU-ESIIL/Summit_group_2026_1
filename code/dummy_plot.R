67195d6bf1c51a80b65a52a139d36fd118c6c797
Beatrice Bock <bmb646@nau.edu>
2026-05-14 13:27:48 -0600

    greater than sign

--- code/dummy_plot.R
+++ code/dummy_plot.R
@@ -10,7 +10,7 @@ ggplot(ds, aes(x = xs, y = ys)) + geom_smooth()+
 geom_point()+
 labs(x = "Knowledge on Energy Usage (Unit of Knowledge)",
     y = "Energy Usage (Unit of Energy Usage)",
-    title = "Team <5'9 Analysis of Energy Knowledge and Energy Usage")+
+    title = "Team >5'9 Analysis of Energy Knowledge and Energy Usage")+
     theme_bw()
 
 ggsave("docs/assets/figures/energy_use.png", width = 6, height = 4)
